8b02b537a746c4478c7b6fc756c4647c18c14b7c
venson <venson.hunt@gmail.com>
2022-06-19 15:11:24 +0800

    [feature] use nacos config for oss

--- service/service_oss/src/main/java/com/venson/oss/OssApplication.java
+++ service/service_oss/src/main/java/com/venson/oss/OssApplication.java
@@ -1,17 +1,19 @@
 package com.venson.oss;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class, SecurityAutoConfiguration.class} )
 @ComponentScan(basePackages = {"com.venson"})
-//@EnableConfigurationProperties
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
+@EnableFeignClients
 public class OssApplication {
     public static void main(String[] args) {
         SpringApplication.run(OssApplication.class,args);
