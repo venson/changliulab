32c072f9040d55e10092115c773d82f1b699c190
venson <venson.hunt@gmail.com>
2022-06-19 14:55:51 +0800

    [feature] add annotation for entity UserRole for mybatis logic delete and auto injection

--- service/service_acl/src/main/java/com/venson/aclservice/entity/User.java
+++ service/service_acl/src/main/java/com/venson/aclservice/entity/User.java
@@ -1,9 +1,8 @@
 package com.venson.aclservice.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.util.Date;
-import com.baomidou.mybatisplus.annotation.TableId;
 import java.io.Serializable;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -38,10 +37,13 @@ public class User implements Serializable {
 
     private String token;
 
+    @TableLogic
     private Boolean isDeleted;
 
+    @TableField(fill = FieldFill.INSERT)
     private Date gmtCreate;
 
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date gmtModified;
 
 
