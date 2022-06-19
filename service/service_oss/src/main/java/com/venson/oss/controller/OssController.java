0b0099f303cceccf5a655201cebf121054a653c2
venson <venson.hunt@gmail.com>
2022-06-19 15:12:12 +0800

    [bug]  add consumes for multipartfile to avoid bug when using springdoc

--- service/service_oss/src/main/java/com/venson/oss/controller/OssController.java
+++ service/service_oss/src/main/java/com/venson/oss/controller/OssController.java
@@ -4,6 +4,7 @@ import com.venson.commonutils.RMessage;
 import com.venson.oss.service.OssService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -16,7 +17,7 @@ public class OssController {
     @Autowired
     private OssService ossService;
 
-    @PostMapping
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public RMessage uploadOssFile(@RequestPart("file") MultipartFile file){
         log.info("start upload");
         String url = ossService.uploadFileAvatar(file);
