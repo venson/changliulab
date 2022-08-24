d058337b3fae800b97e1293addf749627245a7b0
venson <venson.hunt@gmail.com>
2022-08-24 15:56:08 +0800

    [fix] remove unrecommended webSecurity ignoring match

--- common/spring_security/src/main/java/com/venson/security/config/TokenWebSecurityConfig.java
+++ common/spring_security/src/main/java/com/venson/security/config/TokenWebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.venson.security.config;
 
-import com.venson.security.entity.bo.UserContextInfoBO;
 import com.venson.security.filter.TokenAuthenticationFilter;
 import com.venson.security.filter.TokenLoginFilter;
 import com.venson.security.security.TokenLogoutHandler;
@@ -12,15 +11,11 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.io.Serializable;
-import java.util.List;
 
 
 /**
@@ -51,23 +46,17 @@ public class TokenWebSecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.antMatcher("**/admin/**").
         http.authorizeRequests().antMatchers("/").permitAll();
         http.authorizeRequests().antMatchers("/admin/acl/login").permitAll();
+        http.exceptionHandling()
+            .authenticationEntryPoint(new UnauthorizedEntryPoint());
+            http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-//                http.antMatcher("**/admin/**").authorizeRequests()
-//                .anyRequest().hasAuthority("admin");
-
-
-                http.exceptionHandling()
-                .authenticationEntryPoint(new UnauthorizedEntryPoint());
-                http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-                http.csrf().disable()
-                .authorizeRequests().and().logout().logoutUrl("/admin/acl/index/logout")
-                .addLogoutHandler(new TokenLogoutHandler(tokenManager,redisTemplate)).and()
-                .addFilter(new TokenLoginFilter(authenticationManager(), tokenManager, redisTemplate))
-                .addFilter(new TokenAuthenticationFilter(authenticationManager(), tokenManager, redisTemplate)).httpBasic();
+            http.csrf().disable()
+            .authorizeRequests().and().logout().logoutUrl("/admin/acl/index/logout")
+            .addLogoutHandler(new TokenLogoutHandler(tokenManager,redisTemplate)).and()
+            .addFilter(new TokenLoginFilter(authenticationManager(), tokenManager, redisTemplate))
+            .addFilter(new TokenAuthenticationFilter(authenticationManager(), tokenManager, redisTemplate)).httpBasic();
     }
 
     /**
@@ -78,15 +67,4 @@ public class TokenWebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
     }
 
-    /**
-     * 配置哪些请求不拦截
-     */
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/api/**",
-                "/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**"
-                ,"/swagger-ui/**"
-               );
-//        web.ignoring().antMatchers("/*/**");
-    }
 }
