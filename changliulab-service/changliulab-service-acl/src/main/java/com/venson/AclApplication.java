54072f6fabe6b2bd282b12bb6ff741eda1cf486d
venson <venson.hunt@gmail.com>
2022-08-28 20:38:26 +0800

    [fix] pom fix

--- changliulab-service/changliulab-service-acl/src/main/java/com/venson/AclApplication.java
+++ changliulab-service/changliulab-service-acl/src/main/java/com/venson/AclApplication.java
@@ -1,10 +1,15 @@
 package com.venson;
 
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.annotation.NacosProperties;
+import com.alibaba.nacos.api.config.annotation.NacosConfigListener;
+import com.alibaba.nacos.api.config.annotation.NacosConfigurationProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.venson")
