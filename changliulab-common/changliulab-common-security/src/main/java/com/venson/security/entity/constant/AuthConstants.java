a65e0ca8fd9c6c7cd9202f8012a3697c3ee890c8
venson <venson.hunt@gmail.com>
2022-09-25 23:03:58 +0800

    [feature] add path white list to constant.

--- changliulab-common/changliulab-common-security/src/main/java/com/venson/security/entity/constant/AuthConstants.java
+++ changliulab-common/changliulab-common-security/src/main/java/com/venson/security/entity/constant/AuthConstants.java
@@ -1,7 +1,7 @@
-package com.venson.security.entity;
+package com.venson.security.entity.constant;
 
-public final class SecurityConstants {
-    public static final String TOKEN="X-Token";
+public  class AuthConstants {
+    public static final String TokEN="X-Token";
     public static final String ADMIN_PREFIX="ADMIN";
     public static final String USER_PREFIX="USER";
     public static final Long EXPIRE_24H_MS= (long) (24*60*60*1000);
@@ -9,4 +9,7 @@ public final class SecurityConstants {
     public static final Long EXPIRE_24H_S= (long) (24*60*60);
     public static final Long EXPIRE_2DAY_S= (long) (48*60*60);
     public static final String JWT_KEY="changliulab@20220000";
+    public static final String LOGIN_PATH_ADMIN="/auth/admin/login";
+    public static final String LOGIN_PATH_USER="/auth/front/login";
+    public static final String FRONT_PATTERN="/*/front/**";
 }
