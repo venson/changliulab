b5f86dd6c808d4cd46b5d15cfc612423bc33e167
venson <venson.hunt@gmail.com>
2022-09-26 09:41:36 +0800

    [refactor] refactor password relative entity class for consistent

--- changliulab-service/changliulab-service-acl/src/main/java/com/venson/aclservice/entity/vo/ResetPasswordVo.java
+++ changliulab-service/changliulab-service-acl/src/main/java/com/venson/aclservice/entity/vo/ResetPasswordVo.java
@@ -2,11 +2,13 @@ package com.venson.aclservice.entity.vo;
 
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
 public class ResetPasswordVo implements Serializable {
+    @Serial
     private static final long serialVersionUID = 123123L;
-    private String eMail;
+    private String email;
     private String randomPassword;
 }
