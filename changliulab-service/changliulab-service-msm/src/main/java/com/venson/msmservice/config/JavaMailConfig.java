4b28959775f0f0f1c962c5a01dfe98587b6c6d20
venson <venson.hunt@gmail.com>
2022-09-26 10:05:08 +0800

    [feature] disable to use default spring javamail

--- changliulab-service/changliulab-service-msm/src/main/java/com/venson/msmservice/config/JavaMailConfig.java
+++ changliulab-service/changliulab-service-msm/src/main/java/com/venson/msmservice/config/JavaMailConfig.java
@@ -2,19 +2,23 @@ package com.venson.msmservice.config;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
-@Configuration
+//@Configuration
 @Slf4j
+//@ConfigurationProperties(prefix = "spring.mail")
 public class JavaMailConfig {
 
-    @Value("${spring.mail.port}")
+//    @Value("${spring.mail.port}")
     private Integer port;
 
-    @Value("${spring.mail.host}")
+//    @Value("${spring.mail.host}")
     private String host;
+
+    private String username;
 
     @Bean
     public JavaMailSenderImpl javaMailSender(){
