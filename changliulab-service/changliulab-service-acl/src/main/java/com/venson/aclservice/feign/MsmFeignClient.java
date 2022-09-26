9809168ea76219b0c4c538de9f14df60a06860f3
venson <venson.hunt@gmail.com>
2022-09-26 12:15:41 +0800

    [feature] openfeign for msm module

--- changliulab-service/changliulab-service-acl/src/main/java/com/venson/aclservice/feign/MsmFeignClient.java
+++ changliulab-service/changliulab-service-acl/src/main/java/com/venson/aclservice/feign/MsmFeignClient.java
@@ -1,7 +1,7 @@
 package com.venson.aclservice.feign;
 
 import com.venson.aclservice.entity.vo.ResetPasswordVo;
-import com.venson.commonutils.RMessage;
+import com.venson.commonutils.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,5 +9,5 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(value = "service-msm")
 public interface MsmFeignClient {
     @PostMapping("/edumsm/admin/msm/resetEmail")
-    RMessage resetEmail(@RequestBody ResetPasswordVo passwordVo);
+    Result resetEmail(@RequestBody ResetPasswordVo passwordVo);
 }
