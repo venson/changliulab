5534eeeeaa4a81c0f194dd7b0ffef256e1bed578
venson <venson.hunt@gmail.com>
2022-09-26 10:16:06 +0800

    [refactor] use more functional Result instead of RMessage and add more accurate error message

--- changliulab-service/changliulab-service-edu/src/test/java/com/venson/excel/ExcelListener.java
+++ changliulab-service/changliulab-service-edu/src/test/java/com/venson/excel/ExcelListener.java
@@ -1,24 +1,24 @@
 package com.venson.excel;
 
-import com.alibaba.excel.context.AnalysisContext;
-import com.alibaba.excel.event.AnalysisEventListener;
-
-import java.util.Map;
-
-public class ExcelListener extends AnalysisEventListener<Student> {
-
-    @Override
-    public void invoke(Student student, AnalysisContext analysisContext) {
-        System.out.println("====" + student);
-    }
-
-    @Override
-    public void invokeHeadMap(Map<Integer, String> headMap, AnalysisContext context) {
-        System.out.println(headMap);
-    }
-
-    @Override
-    public void doAfterAllAnalysed(AnalysisContext analysisContext) {
-
-    }
-}
+//import com.alibaba.excel.context.AnalysisContext;
+//import com.alibaba.excel.event.AnalysisEventListener;
+//
+//import java.util.Map;
+//
+//public class ExcelListener extends AnalysisEventListener<Student> {
+//
+//    @Override
+//    public void invoke(Student student, AnalysisContext analysisContext) {
+//        System.out.println("====" + student);
+//    }
+//
+//    @Override
+//    public void invokeHeadMap(Map<Integer, String> headMap, AnalysisContext context) {
+//        System.out.println(headMap);
+//    }
+//
+//    @Override
+//    public void doAfterAllAnalysed(AnalysisContext analysisContext) {
+//
+//    }
+//}
