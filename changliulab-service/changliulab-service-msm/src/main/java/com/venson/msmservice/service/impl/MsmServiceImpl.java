b2bda5b31e238489354ce1870c486ae96d3f9ccc
venson <venson.hunt@gmail.com>
2022-09-26 10:16:50 +0800

    [refactor] add more useage for sendCode method

--- changliulab-service/changliulab-service-msm/src/main/java/com/venson/msmservice/service/impl/MsmServiceImpl.java
+++ changliulab-service/changliulab-service-msm/src/main/java/com/venson/msmservice/service/impl/MsmServiceImpl.java
@@ -2,6 +2,7 @@ package com.venson.msmservice.service.impl;
 
 import com.venson.msmservice.service.MsmService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
@@ -17,27 +19,34 @@ import java.util.HashMap;
 public class MsmServiceImpl implements MsmService  {
 
     @Autowired
-    private JavaMailSenderImpl sender;
+    private JavaMailSender sender;
     @Autowired
     private SpringTemplateEngine springTemplateEngine;
 
 
     @Override
-    public boolean sendCode(String emailUrl, String code, String type ){
+    public boolean sendCode(String emailUrl, String code,String codeName, String mailPurpose,Integer expire){
         MimeMessage mimeMessageMsg = sender.createMimeMessage();
-        String subject = "Changliu Lab " + type ;
+        String subject = "Changliu Lab " + mailPurpose ;
 
         try {
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessageMsg,MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessageMsg,
+                    MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
             HashMap<String,Object> values = new HashMap<>();
-            values.put("type",type);
+            values.put("codeName",codeName);
             values.put("code",code);
+            values.put("mailPurpose",mailPurpose);
+            values.put("expire", expire);
             Context context = new Context();
             context.setVariables(values);
             // use template to generate email context
             String htmlPage = springTemplateEngine.process("sendCode.html", context);
             helper.setTo(emailUrl);
-            helper.setFrom(sender.getUsername());
+            try {
+                helper.setFrom("info@changliulab.com","Changliulab Admin");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
             helper.setSubject(subject);
             helper.setText(htmlPage,true);
         } catch (MessagingException e) {
