0d1138dfec31f7c1a62abaaf8e2857243499d644
venson <venson.hunt@gmail.com>
2023-02-19 22:25:05 +0800

    [feature] add more precise exception handle

--- changliulab-common/changliulab-common-service/src/main/java/com/venson/servicebase/exceptionhandler/GlobalExceptionHandler.java
+++ changliulab-common/changliulab-common-service/src/main/java/com/venson/servicebase/exceptionhandler/GlobalExceptionHandler.java
@@ -4,30 +4,47 @@ import com.venson.commonutils.Result;
 import com.venson.servicebase.exception.CustomizedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    @ResponseBody
-    public Result error(Exception e){
-        e.printStackTrace();
-        if(e instanceof AccessDeniedException){
-            return Result.error().message("Not Enough Authentication");
-        }else{
-            return Result.error().message("Server Error");
-        }
+    @ExceptionHandler(AccessDeniedException.class)
+    public Result<String> AccessDeniedError(Exception e){
+            return Result.error(e.getMessage());
     }
 
     @ExceptionHandler(CustomizedException.class)
-    @ResponseBody
-    public Result CustomizedExceptionError(CustomizedException e){
-        e.printStackTrace();
-        return Result.error().message(e.getMessage()).data(e.getCode().toString(), e.getMsg());
+    public Result<String> CustomizedExceptionError(CustomizedException e){
+        return Result.error(e.getMessage());
+    }
+    @ExceptionHandler({IllegalStateException.class, IllegalArgumentException.class})
+    public Result<String> IllegalExceptionError(Exception e){
+        return Result.error(e.getMessage());
+    }
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public Result<String> methodArgumentError(MethodArgumentNotValidException e){
+        BindingResult bindingResult = e.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        StringBuilder message = new StringBuilder();
+
+        for (FieldError error :
+                fieldErrors) {
+            message.append(error.getDefaultMessage()).append(". ");
+        }
+        return Result.error(message.toString());
+    }
+    @ExceptionHandler(Exception.class)
+    public Result<String> error(Exception e){
+        log.error("Exception",e);
+        return Result.error("Server Error");
     }
 }
