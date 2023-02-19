855c3dfc32d17c536a5d0e5ad78701f08d514a0d
venson <venson.hunt@gmail.com>
2023-02-20 02:25:24 +0800

    [refactor] refactor front controller

--- changliulab-service/changliulab-service-msm/src/main/java/com/venson/msmservice/controller/FrontMsmController.java
+++ changliulab-service/changliulab-service-msm/src/main/java/com/venson/msmservice/controller/FrontMsmController.java
@@ -1,57 +1,44 @@
 package com.venson.msmservice.controller;
 
-import com.venson.commonutils.RandomString;
 import com.venson.commonutils.Result;
-import com.venson.msmservice.service.MsmService;
+import com.venson.msmservice.entity.enums.SendStatus;
+import com.venson.msmservice.service.MsmFrontService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/edumsm/front/msm")
 @Slf4j
 public class FrontMsmController {
+    private final MsmFrontService msmFrontService;
+
     @Autowired
-    private  MsmService msmService;
-    @Autowired
-    private StringRedisTemplate redisTemplate;
+    public FrontMsmController(MsmFrontService msmFrontService) {
+        this.msmFrontService = msmFrontService;
+    }
 
 
     @PostMapping(value = "securityCode")
-    public Result sendEmail(@RequestBody String emailUrl){
-        emailUrl = emailUrl.trim().replace("\"", "");
-        if(!StringUtils.hasText(emailUrl)){
-            return Result.error().message("email is null");
+    public Result<String> sendEmail(@RequestBody String emailUrl){
+        SendStatus status = msmFrontService.getSecurityCode(emailUrl);
+        switch (status){
+            case SUCCESS -> {
+                return Result.success();
+            }
+            case TOO_FREQUENTLY -> {
+                return Result.error("Please try after 5 minutes");
+            }
+            case TOO_MANY_ATTEMPTS -> {
+                return Result.error("3 attempts per day, Please try tomorrow");
+            }
+            default -> {
+                return Result.error();
+            }
         }
-        int count =0;
-        String emailUrlCount = emailUrl + ":count";
-        log.info("Security Code to Email:" + emailUrl);
-        Long expireTime = redisTemplate.getExpire(emailUrl, TimeUnit.MINUTES);
-        String s = redisTemplate.opsForValue().get(emailUrlCount);
-        if(s!=null){
-            count = Integer.parseInt(s);
-        }
-        if(count>=3){
-            return Result.error().message("You have sent security code for the third time, please try tomorrow");
-        }
-        if(expireTime!=null && expireTime>= 1){
-            return Result.error().message("Please Wait");
-        }
-        String code = RandomString.randomCode();
-        boolean result = msmService.sendCode(emailUrl, code,"Security Code",
-                "Registration Security Code",20);
-        if (result){
-            redisTemplate.opsForValue().set(emailUrl, code, 20, TimeUnit.MINUTES);
-            redisTemplate.opsForValue().increment(emailUrlCount);
-            return Result.success().message("Security Code Sent, " +(2-count) + "times left for today");
-        }
-        return Result.error();
     }
 }
